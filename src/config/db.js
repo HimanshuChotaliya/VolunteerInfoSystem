6b18222eb7ea50d2f58ffca657937c2e41b3064b
Himanshu Chotaliya <himanshu746h@gmail.com>
2026-06-17 21:58:49 +0530

    initial commmit

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = process.env.DATABASE_URL
-  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  ? new Pool({ connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+   })
   : new Pool({
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
